33335c6bd65dae62b4f0130dff426a8eace9fc24
Ved Prakash <vedprakash995664@gmail.com>
2024-11-14 16:19:03 +0530

    some minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import WOW from 'wowjs';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
